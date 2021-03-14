a38cc19a8757480be751583309d1ce1b9f1ffa58
ChristianMocha <letmy.4@hotmail.com>
2021-03-14 16:12:27 -0500

    header redondeados and tringulares

--- lib/src/widgets/headers.dart
+++ lib/src/widgets/headers.dart
@@ -29,8 +29,79 @@ class HeaderDiagonal extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 200,
-      color: Color(0xff00C1E3),
+      height: double.infinity,
+      width: double.infinity,
+      // color: Color(0xff00C1E3),
+      child: CustomPaint(
+        painter: _headerDiagonalPainter(),
+      ),
     );
+  }
+}
+
+class _headerDiagonalPainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    final lapiz = new Paint();
+
+    // propiedadess
+    lapiz.color = Color(0xff00C1E3);
+    lapiz.style = PaintingStyle.fill; //.fill
+    lapiz.strokeWidth = 2;
+
+    final path = new Path();
+
+    // Dibujar con el path y el lapiz
+    path.moveTo(0, size.height * .35);
+    path.lineTo(size.width, size.height * .25);
+    path.lineTo(size.width, 0);
+    path.lineTo(0, 0);
+
+    canvas.drawPath(path, lapiz);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return true;
+  }
+}
+
+class HeaderTriangular extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: double.infinity,
+      width: double.infinity,
+      // color: Color(0xff00C1E3),
+      child: CustomPaint(
+        painter: _headerTriangularPainter(),
+      ),
+    );
+  }
+}
+
+class _headerTriangularPainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    final lapiz = new Paint();
+
+    // propiedadess
+    lapiz.color = Color(0xff00C1E3);
+    lapiz.style = PaintingStyle.fill; //.fill
+    lapiz.strokeWidth = 5;
+
+    final path = new Path();
+
+    // Dibujar con el path y el lapiz
+    path.moveTo(size.width, size.height);
+    path.lineTo(size.width, 0);
+    path.lineTo(0, 0);
+
+    canvas.drawPath(path, lapiz);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return true;
   }
 }
