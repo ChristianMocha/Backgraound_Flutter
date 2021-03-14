6edb95b23b774fab5a7b7899ae19a71a24fbe628
ChristianMocha <letmy.4@hotmail.com>
2021-03-14 16:44:07 -0500

    header wave alrevez

--- lib/src/widgets/headers.dart
+++ lib/src/widgets/headers.dart
@@ -231,3 +231,47 @@ class _headerWavePainter extends CustomPainter {
     return true;
   }
 }
+
+class HeaderWaveAlrevez extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: double.infinity,
+      width: double.infinity,
+      // color: Color(0xff00C1E3),
+      child: CustomPaint(
+        painter: _headerWaveAlrevezPainter(),
+      ),
+    );
+  }
+}
+
+class _headerWaveAlrevezPainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    final lapiz = new Paint();
+
+    // propiedadess
+    lapiz.color = Color(0xff00C1E3);
+    lapiz.style = PaintingStyle.fill; //.fill .stroke
+    lapiz.strokeWidth = 5;
+
+    final path = new Path();
+
+    // Dibujar con el path y el lapiz
+    path.moveTo(0, size.height * .2);
+    path.quadraticBezierTo(
+        size.width * .20, size.height * .3, size.width * .5, size.height * .2);
+    path.quadraticBezierTo(
+        size.width * .8, size.height * .1, size.width, size.height * .2);
+    path.lineTo(size.width, size.height);
+    path.lineTo(0, size.height);
+
+    canvas.drawPath(path, lapiz);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return true;
+  }
+}
