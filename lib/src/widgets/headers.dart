de6ccef8fe4c26d1b4d0c26075d485615ebfc883
ChristianMocha <letmy.4@hotmail.com>
2021-03-14 16:31:33 -0500

    header curvo

--- lib/src/widgets/headers.dart
+++ lib/src/widgets/headers.dart
@@ -147,3 +147,44 @@ class _headerPicoPainter extends CustomPainter {
     return true;
   }
 }
+
+class HeaderCurvo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: double.infinity,
+      width: double.infinity,
+      // color: Color(0xff00C1E3),
+      child: CustomPaint(
+        painter: _headerCurvoPainter(),
+      ),
+    );
+  }
+}
+
+class _headerCurvoPainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    final lapiz = new Paint();
+
+    // propiedadess
+    lapiz.color = Color(0xff00C1E3);
+    lapiz.style = PaintingStyle.fill; //.fill .stroke
+    lapiz.strokeWidth = 5;
+
+    final path = new Path();
+
+    // Dibujar con el path y el lapiz
+    path.lineTo(0, size.height * .20);
+    path.quadraticBezierTo(
+        size.width * .5, size.height * .4, size.width, size.height * .20);
+    path.lineTo(size.width, 0);
+
+    canvas.drawPath(path, lapiz);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return true;
+  }
+}
